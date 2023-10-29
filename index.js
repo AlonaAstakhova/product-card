c8bd1c56edbc5edfa876f86230810933411a9fad
AlonaAstakhova <alyonasta@gmail.com>
2023-10-28 17:13:19 -0700

    feat: click event for color  buttons

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ fetch('./data.json')
   .then((data) => {
     let sizeList = []
     let colorList = []
-
     let disColorList = []
     let firstColor = ''
     const prices = {}
@@ -12,7 +11,6 @@ fetch('./data.json')
     //переписать список размеров и цветов на Set
 
     //Формируем списки всех существующих размеров и цветов
-
     data.forEach((el) => {
       const size = el.size.value
       const color = el.color.value
@@ -89,13 +87,11 @@ fetch('./data.json')
         if (el.size.value === firstSize && data[i].qty > 0) {
           if (!colorListFirstSize.includes(el.color.value)) {
             colorListFirstSize = [...colorListFirstSize, el.color.value]
-            console.log('colorListFirstSize: ' + colorListFirstSize)
           }
         }
         disColorList = colorList.filter(
           (el) => !colorListFirstSize.includes(el)
         )
-        console.log('disColorList: ' + disColorList)
       })
 
       //Делаем неактивными кнопки цветов, которые недоступны для первого cуществующего размера и устанавливаем "disabled"
@@ -111,14 +107,25 @@ fetch('./data.json')
     disabledColor(data, firstSize)
 
     //Устанавливаем цену
-    document.querySelector('.price').textContent = '$ ' + prices.validPrice
-    if (prices.validPrice !== prices.inValidPrice) {
-      document.querySelector('.price-before').textContent =
-        '$ ' + prices.inValidPrice
+    function price() {
+      document.querySelector('.price').textContent = '$ ' + prices.validPrice
+
+      if (prices.validPrice !== prices.inValidPrice) {
+        document.querySelector('.price-before').textContent =
+          '$ ' + prices.inValidPrice
+      }
+      // else {
+      // document.querySelector('.price-before').textContent =
+      //   '$ ' + prices.inValidPrice
+      // document.querySelector('.price-before').style.color = 'rgb(255,255,255)'
+      // document.querySelector('.price-before').style.textDecoration = 'none'
+      // }
     }
 
+    price()
+
     //Обработка клика клиента по кнопкам размера
-    const onButton = (event) => {
+    const onButtonSize = (event) => {
       let sizeClick = event.target.dataset.value
       if (sizeClick) {
         //снять активный статус с других кнопок, снять статус disabled
@@ -141,14 +148,56 @@ fetch('./data.json')
         //находим первую доступную опцию во втором ряду и делаем ее активной
         frColor(data, sizeClick)
         //показываем цены для выбранного размера и цвета
+        price()
       }
-      console.log(event)
-
-      //та же самая логика, но если клиент выбирает цвет
     }
     document
       .querySelector("[data-js='size']")
-      .addEventListener('click', onButton)
+      .addEventListener('click', onButtonSize)
+
+    //Обработка клика клиента по кнопкам размера
+    const onButtonColor = (event) => {
+      let colorClick = event.target.dataset.value
+      if (colorClick) {
+        //убрать цены
+        document.querySelector('.price-before').textContent = ''
+        document.querySelector('.price').textContent = ''
+
+        if (
+          !document
+            .querySelector(`[data-value='${colorClick}']`)
+            .classList.contains('disabled')
+        ) {
+          //снять активный статус с других кнопок цвета
+          document.querySelectorAll('.color').forEach((color) => {
+            color.classList.remove('active')
+          })
+          //ставим активный статус на выбраную кнопку
+          document
+            .querySelector(`[data-value='${colorClick}']`)
+            .classList.add('active')
+        }
+        //находим верную цену для выбранного цвета и размера
+        document.querySelectorAll('.size').forEach((size) => {
+          if (size.classList.contains('active')) {
+            data.filter((el) => {
+              if (
+                el.color.value === colorClick &&
+                el.size.value === size.textContent
+              ) {
+                prices['validPrice'] = el.valid_price
+                prices['inValidPrice'] = el.invalid_price
+              }
+            })
+          }
+        })
+        //показываем цены для выбранного размера и цвета
+        price()
+      }
+    }
+    document
+      .querySelector("[data-js='color']")
+      .addEventListener('click', onButtonColor)
   })
 
   .catch((error) => console.log(error))
